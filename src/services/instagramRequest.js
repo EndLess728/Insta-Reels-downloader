0550d1ff50b11ab069c23508e7231c47d5c7bda4
EndLess728 <aryanmk.mk5@gmail.com>
2021-03-16 20:56:42 +0530

    Use static User-Agent instead of Dynamic user-agent

--- src/services/instagramRequest.js
+++ src/services/instagramRequest.js
@@ -7,14 +7,15 @@ export default class InstagramRequest {
         return ua.osName === 'Android';
       });
       const headers = {
-        'User-Agent': userAgent,
+        'User-Agent':
+          'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36',
       };
       fetch(url, {headers})
         .then((res) => res.json())
         .then((requstData) => {
           resolve(requstData.graphql.shortcode_media);
         })
-        .catch((err) => reject('ERROR!'));
+        .catch((err) => reject(JSON.stringify(err)));
     });
   }
 }
