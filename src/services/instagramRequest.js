450e245c40b129ffe9e338082547ae2e83d9e28b
Mantu <=_=>
2021-03-22 15:26:54 +0530

    Add new user-agent module for latest User-Agent

--- src/services/instagramRequest.js
+++ src/services/instagramRequest.js
@@ -1,14 +1,14 @@
-import randomUseragent from 'random-useragent';
+import UserAgent from 'user-agents';
 
 export default class InstagramRequest {
   static getFileData(url) {
     return new Promise((resolve, reject) => {
-      const userAgent = randomUseragent.getRandom(function (ua) {
-        return ua.osName === 'Android';
+      const userAgent = new UserAgent({
+        deviceCategory: 'mobile',
+        platform: 'Linux armv8l',
       });
       const headers = {
-        'User-Agent':
-          'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36',
+        'User-Agent': userAgent.toString(),
       };
       fetch(url, {headers})
         .then((res) => res.json())
